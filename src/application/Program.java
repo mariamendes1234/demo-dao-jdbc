192d3d3363ab597f898ed46b941f51ba01dd21e6
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-03 12:06:20 +0100

    SellerDaoJDBC e Daofactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,9 @@ public class Program {
 		Department obj = new Department(1,"books");
 		System.out.println(obj);
 		
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println(sellerDao);
 	}
 
 }
