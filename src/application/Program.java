8a38c3931f648b93e496de8e4c9a424f334e2901
Maria Mendes <mendes.maria@ipcbcampus.pt>
2024-09-03 12:38:51 +0100

    Reutilizando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ public class Program {
 		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+		System.out.println("===Test 1: seller by id: ");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
